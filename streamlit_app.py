1a4eb0cce59be1f553936f8e50a0a0132dfaea68
Emmanuel Siyauya <manelisiyauya2005@gmail.com>
2026-06-20 16:28:18 +0300

    Add Streamlit dashboard for serving cleaned air quality data

---
+++ streamlit_app.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import pandas as pd
+import sqlite3
+
+conn = sqlite3.connect(r"C:\Users\Quantam Waffle\OneDrive\Desktop\Data engineering and management\Data engineering project\widetable.db")
+
+
+
+df = pd.read_sql(
+    "SELECT * FROM pollutant_wide",
+    conn
+)
+
+
+st.title("Air Quality Monitoring Dashboard")
+
+st.write("Cleaned Wide-Table Dataset")
+
+st.dataframe(df.head(100))
+
+
+st.subheader("Dataset Shape")
+
+st.write(
+    f"Rows: {df.shape[0]}"
+)
+
+st.write(
+    f"Columns: {df.shape[1]}"
+)
+
+pollutants = [
+    "benzene",
+    "co",
+    "nh3",
+    "no",
+    "no2",
+    "ozone",
+    "pm10",
+    "pm25",
+    "so2",
+    "toluene"
+]
+
+selected = st.selectbox(
+    "Select Pollutant",
+    pollutants
+)
+
+st.subheader(
+    f"{selected} Statistics"
+)
+
+st.write(
+    df[selected].describe()
+)
+
+st.line_chart(df[selected])
